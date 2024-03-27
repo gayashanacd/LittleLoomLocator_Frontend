ad33b1501a0cc6e7e2a602ed48f20b9ba6fe0135
Subasinghe Mudiyanselage <subasinghemudii@student.douglascollege.ca>
2024-03-27 10:16:59 -0700

    added bootstrap theme

--- src/components/user/UserLogin.vue
+++ src/components/user/UserLogin.vue
@@ -1,18 +1,73 @@
 <template>
     <div>
-        <h4>Login</h4>
-        <form>
-            <div>
-                <label for="studentId">Student ID</label>
-                <input type="text" id="studentId" v-model="studentLoginRequest.studentId" />
+        <main>
+            <div class="container">
+
+            <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+                <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+                    <div class="d-flex justify-content-center py-4">
+                        <a href="index.html" class="logo d-flex align-items-center w-auto">
+                        <img src="assets/logo.png" alt="">
+                        <span class="d-none d-lg-block">LittleLoom Locator</span>
+                        </a>
+                    </div><!-- End Logo -->
+
+                    <div class="card mb-3">
+
+                        <div class="card-body">
+
+                        <div class="pt-4 pb-2">
+                            <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
+                            <p class="text-center small">Enter your username & password to login</p>
+                        </div>
+
+                        <form class="row g-3 needs-validation" novalidate>
+
+                            <div class="col-12">
+                            <label for="yourUsername" class="form-label">Username</label>
+                            <div class="input-group has-validation">
+                                <span class="input-group-text" id="inputGroupPrepend">@</span>
+                                <input type="text" name="username" class="form-control" id="yourUsername" v-model="usertLoginRequest.username" required>
+                                <div class="invalid-feedback">Please enter your username.</div>
+                            </div>
+                            </div>
+
+                            <div class="col-12">
+                            <label for="yourPassword" class="form-label">Password</label>
+                            <input type="password" name="password" class="form-control" id="yourPassword" v-model="usertLoginRequest.password" required>
+                            <div class="invalid-feedback">Please enter your password!</div>
+                            </div>
+
+                            <div class="col-12">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="remember" value="true" id="rememberMe">
+                                <label class="form-check-label" for="rememberMe">Remember me</label>
+                            </div>
+                            </div>
+                            <div class="col-12">
+                            <button class="btn btn-primary w-100" type="submit" @click="login">Login</button>
+                            </div>
+                            <div class="col-12">
+                            <p class="small mb-0">Don't have account? <a href="pages-register.html">Create an account</a></p>
+                            </div>
+                        </form>
+
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+
+            </section>
+
             </div>
-            <div>
-                <label for="password">Password</label>
-                <input type="password" id="password" v-model="studentLoginRequest.password" />
-            </div>
-        </form>
-        <button type="submit" @click="login">Login</button>
-        <p>{{ message }}</p>
+        </main><!-- End #main -->
+
+        <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
     </div>
 </template>
 
@@ -20,17 +75,17 @@
 import LoginService from "@/services/LoginService";
 
 export default {
-    name: "studentLogin",
+    name: "UserLogin",
     data() {           
         return {
-            studentLoginRequest: { studentId: "", password: "" },  
+            usertLoginRequest: { username: "", password: "" },  
             message: ""
         };
     },
     methods: {
         login(event) {
             event.preventDefault();
-            LoginService.login(this.studentLoginRequest)
+            LoginService.login(this.usertLoginRequest)
                 .then(response => {       
                     var student = response.data;
                     console.log(student);
@@ -39,8 +94,8 @@ export default {
                     this.$router.push({ name: "studentProfile" });
                 })
                 .catch(e => {
-                    this.studentLoginRequest.studentId = "";
-                    this.studentLoginRequest.password = "";
+                    this.usertLoginRequest.studentId = "";
+                    this.usertLoginRequest.password = "";
                     this.message = e.response.data.message;
                     console.log(e.response.data);
                 });
