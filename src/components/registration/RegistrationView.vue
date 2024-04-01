fe89b24654ed6a4b531abcd4122737bd3fef9191
nelumdm <dissanayakemudn@student.douglascollege.ca>
2024-04-01 02:20:46 -0700

    Added Registration Component

---
+++ src/components/registration/RegistrationView.vue
@@ -0,0 +1,122 @@
+<template>
+    <div>
+        <main>
+            <div class="container">
+
+            <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+                <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+                    <div class="d-flex justify-content-center py-4">
+                        <a href="index.html" class="logo d-flex align-items-center w-auto">
+                        <img alt="Logo" src="@/assets/logo.png">
+                        <!-- <span class="d-none d-lg-block">LittleLoom Locator</span> -->
+                        </a>
+                    </div><!-- End Logo -->
+
+                    <div class="card mb-3">
+
+                        <div class="card-body">
+
+                        <div class="pt-4 pb-2">
+                            <h5 class="card-title text-center pb-0 fs-4">Register</h5>
+                            <!-- <p class="text-center small">Enter your username & password to login</p> -->
+                        </div>
+
+                        <form class="row g-3 needs-validation" novalidate>
+
+                            <div class="col-12">
+                            <label for="yourUsername" class="form-label">Username</label>
+                            <div class="input-group has-validation">
+                                <span class="input-group-text" id="inputGroupPrepend">@</span>
+                                <input type="text" name="username" class="form-control" id="yourUsername" v-model="usertLoginRequest.username" required>
+                                <div class="invalid-feedback">Please enter your username.</div>
+                            </div>
+                            </div>
+
+                            <div class="col-12">
+                            <label for="yourPassword" class="form-label">Password</label>
+                            <input type="password" name="password" class="form-control" id="yourPassword" v-model="usertLoginRequest.password" required>
+                            <div class="invalid-feedback">Please enter your password!</div>
+                            </div>
+
+                            <div class="col-12">
+                            <div class="form-select">
+                                <select class="form-select" name="userType">
+                                    <option selected="">Select the type</option>
+                                    <option value="parent">Parent</option>
+                                    <option value="institute">Institute</option>
+                                <!-- <option value="3">Three</option> -->
+                                </select>
+                            </div>
+                            </div>
+                            <div class="col-12">
+                            <button class="btn btn-primary w-100" type="submit">Register</button>
+                            </div>
+                            <div class="col-12">
+                            <a href="pages-register.html">Back to Login</a>
+                            </div>
+                        </form>
+
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+
+            </section>
+
+            </div>
+        </main><!-- End #main -->
+
+        <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    </div>
+</template>
+
+<script>
+// import LoginService from "@/services/LoginService";
+
+export default {
+    name: "UserLogin",
+    data() {           
+        return {
+            usertLoginRequest: { username: "", password: "" },  
+            message: ""
+        };
+    },
+    methods: {
+        login(event) {
+            event.preventDefault();
+            // LoginService.login(this.usertLoginRequest)
+            //     .then(response => {       
+            //         var student = response.data;
+            //         console.log(student);
+            //         localStorage.setItem("sid", student.id);
+            //         this.message = student;
+            //         this.$router.push({ name: "studentProfile" });
+            //     })
+            //     .catch(e => {
+            //         this.usertLoginRequest.studentId = "";
+            //         this.usertLoginRequest.password = "";
+            //         this.message = e.response.data.message;
+            //         console.log(e.response.data);
+            //     });
+            this.$util.setAuth(true);
+            this.$router.push({ name: "ParentView" });    
+            location.reload();   
+        }
+    },
+    mounted() {   
+        this.message = "";
+    }
+};
+</script>
+
+<style>
+.edit-form {
+    max-width: 300px;
+    margin: auto;
+}
+</style>
